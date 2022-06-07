c9ddbbeac23b4241998da809cbf2dab229767f3e
AdrianoAnimus <adrianoanimus@gmail.com>
2022-06-06 22:36:15 -0300

    Create Component "Content".

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,42 +1,15 @@
 
 import './App.css';
 import Main from './Main';
+import Conteudo from './Conteudo';
 
 function App() {
   return (
     <div className="App">
       <Main/>
  
-      <div className="conteudo">
-        <div className="center">
-          <div className="conteudo-single">
-            <h3>Titulo da Chamada</h3>
-            <p>Espaço destinado aos conteudos para melhor definição da Landing Page
-              criada por Adriano Pereira, desenvolvedor Front-end
-              em formação.</p>
-          </div>
-          <div className="conteudo-single">
-            <h3>Titulo da Chamada</h3>
-            <p>Espaço destinado aos conteudos para melhor definição da Landing Page
-              criada por Adriano Pereira, desenvolvedor Front-end
-              em formação.</p>
-          </div>
-          <div className="conteudo-single">
-            <h3>Titulo da Chamada</h3>
-            <p>Espaço destinado aos conteudos para melhor definição da Landing Page
-              criada por Adriano Pereira, desenvolvedor Front-end
-              em formação.</p>
-          </div>
-          <div className="conteudo-single">
-            <h3>Titulo da Chamada</h3>
-            <p>Espaço destinado aos conteudos para melhor definição da Landing Page
-              criada por Adriano Pereira, desenvolvedor Front-end
-              em formação.</p>
-          </div>
-        </div>
+      <Conteudo/>
       </div>
-
-    </div>
   );
 }
 
---
+++ projeto/src/Conteudo.js
@@ -0,0 +1,34 @@
+function Conteudo(){
+    return(
+        <div className="conteudo">
+        <div className="center">
+          <div className="conteudo-single">
+            <h3>Titulo da Chamada</h3>
+            <p>Espaço destinado aos conteudos para melhor definição da Landing Page
+              criada por Adriano Pereira, desenvolvedor Front-end
+              em formação.</p>
+          </div>
+          <div className="conteudo-single">
+            <h3>Titulo da Chamada</h3>
+            <p>Espaço destinado aos conteudos para melhor definição da Landing Page
+              criada por Adriano Pereira, desenvolvedor Front-end
+              em formação.</p>
+          </div>
+          <div className="conteudo-single">
+            <h3>Titulo da Chamada</h3>
+            <p>Espaço destinado aos conteudos para melhor definição da Landing Page
+              criada por Adriano Pereira, desenvolvedor Front-end
+              em formação.</p>
+          </div>
+          <div className="conteudo-single">
+            <h3>Titulo da Chamada</h3>
+            <p>Espaço destinado aos conteudos para melhor definição da Landing Page
+              criada por Adriano Pereira, desenvolvedor Front-end
+              em formação.</p>
+          </div>
+        </div>
+      </div>    
+    )
+}
+
+export default Conteudo
